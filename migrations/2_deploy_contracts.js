a99accfb921bc6e77e4865fd26588eba4a9bfcbd
devilla <dev.koold@gmail.com>
2021-01-27 14:22:16 +0530

    ISC licence and project settings

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,4 +2,15 @@ const Storage = artifacts.require("Storage");
 
 module.exports = function (deployer) {
   deployer.deploy(Storage);
+
+  // Deploy Dai
+  await deployer.deploy(Dai, '5777');
+  const dai = await Dai.deployed();
+
+  //Deploy EthSwap with Dai
+  await deployer.deploy(EthSwap, dai.address);
+  ethSwap =  await EthSwap.deployed();
+  // Mint 0.001 Dai to EthSwap
+  await dai.transfer(ethSwap.address, '1000000000000000000000000');
+
 };
